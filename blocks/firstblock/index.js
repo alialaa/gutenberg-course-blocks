18c2f060b31f62995ffeda2d4aaac03e8a794838
digital ankit <akjaph@gmail.com>
2019-04-13 17:08:22 +0530

    created a new plugin and registered a block

---
+++ blocks/firstblock/index.js
@@ -0,0 +1,14 @@
+var registerBlockType = wp.block.registerBlockType;
+var __ = wp.i18n.__;
+
+registerBlockType('mytheme-blocks/firstblock', {
+    title: __('First Block', 'mytheme-blocks'),
+    description: __('Our first block', 'mytheme-blocks'),
+    category: 'layout',
+    edit: function() {
+        return 'Editor';
+    },
+    save: function() {
+        return 'Saved content';
+    }
+})
---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+* Plugin Name: mytheme-blocks
+* Plugin URI: https://alialaa.com/
+* Description: Blocks for mytheme.
+* Author: alialla 
+* Author URI https://alialaa.com/
+*/

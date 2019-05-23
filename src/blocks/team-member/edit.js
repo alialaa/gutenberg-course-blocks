5a6529456cf8da1aabf7e81b6f7fa2efcf99f24f
digital ankit <akjaph@gmail.com>
2019-05-23 06:25:05 +0530

    added edit image and remove image buttons and functionality

--- src/blocks/team-member/edit.js
+++ src/blocks/team-member/edit.js
@@ -1,8 +1,19 @@
 import { Component } from "@wordpress/element";
-import { RichText, MediaPlaceholder } from "@wordpress/editor";
+import {
+    RichText,
+    MediaPlaceholder,
+    BlockControls,
+    MediaUpload,
+    MediaUploadCheck
+} from "@wordpress/editor";
 import { __ } from "@wordpress/i18n";
 import { isBlobURL } from "@wordpress/blob";
-import { Spinner, withNotices } from "@wordpress/components";
+import {
+    Spinner,
+    withNotices,
+    Toolbar,
+    IconButton
+} from "@wordpress/components";
 
 class TeamMemberEdit extends Component {
     componentDidMount() {
@@ -39,45 +50,88 @@ class TeamMemberEdit extends Component {
         const { noticeOperations } = this.props;
         noticeOperations.createErrorNotice(message);
     };
+    removeImage = () => {
+        this.props.setAttributes({
+            id: null,
+            url: "",
+            alt: ""
+        });
+    };
     render() {
         //console.log(this.props);
         const { className, attributes, noticeUI } = this.props;
-        const { title, info, url, alt } = attributes;
+        const { title, info, url, alt, id } = attributes;
         return (
-            <div className={className}>
-                {url ? (
-                    <>
-                        <img src={url} alt={alt} />
-                        {isBlobURL(url) && <Spinner />}
-                    </>
-                ) : (
-                    <MediaPlaceholder
-                        icon="format-image"
-                        onSelect={this.onSelectImage}
-                        onSelectURL={this.onSelectURL}
-                        onError={this.onUploadError}
-                        //accept="image/*"
-                        allowedTypes={["image"]}
-                        notices={noticeUI}
+            <>
+                <BlockControls>
+                    {url && (
+                        <Toolbar>
+                            {id && (
+                                <MediaUploadCheck>
+                                    <MediaUpload
+                                        onSelect={this.onSelectImage}
+                                        allowedTypes={["image"]}
+                                        value={id}
+                                        render={({ open }) => {
+                                            return (
+                                                <IconButton
+                                                    className="components-icon-button components-toolbar__control"
+                                                    label={__(
+                                                        "Edit Image",
+                                                        "mytheme-blocks"
+                                                    )}
+                                                    onClick={open}
+                                                    icon="edit"
+                                                />
+                                            );
+                                        }}
+                                    />
+                                </MediaUploadCheck>
+                            )}
+                            <IconButton
+                                className="components-icon-button components-toolbar__control"
+                                label={__("Remove Image", "mytheme-blocks")}
+                                onClick={this.removeImage}
+                                icon="trash"
+                            />
+                        </Toolbar>
+                    )}
+                </BlockControls>
+                <div className={className}>
+                    {url ? (
+                        <>
+                            <img src={url} alt={alt} />
+                            {isBlobURL(url) && <Spinner />}
+                        </>
+                    ) : (
+                        <MediaPlaceholder
+                            icon="format-image"
+                            onSelect={this.onSelectImage}
+                            onSelectURL={this.onSelectURL}
+                            onError={this.onUploadError}
+                            //accept="image/*"
+                            allowedTypes={["image"]}
+                            notices={noticeUI}
+                        />
+                    )}
+                    <RichText
+                        className={"wp-block-mytheme-blocks-team-member__title"}
+                        tagName="h4"
+                        onChange={this.onChangeTitle}
+                        value={title}
+                        placeholder={__("Member Name", "mytheme-blocks")}
+                        formatingControls={[]}
                     />
-                )}
-                <RichText
-                    className={"wp-block-mytheme-blocks-team-member__title"}
-                    tagName="h4"
-                    onChange={this.onChangeTitle}
-                    value={title}
-                    placeholder={__("Member Name", "mytheme-blocks")}
-                    formatingControls={[]}
-                />
-                <RichText
-                    className={"wp-block-mytheme-blocks-team-member__info"}
-                    tagName="p"
-                    onChange={this.onChangeInfo}
-                    value={info}
-                    placeholder={__("Member Info", "mytheme-blocks")}
-                    formatingControls={[]}
-                />
-            </div>
+                    <RichText
+                        className={"wp-block-mytheme-blocks-team-member__info"}
+                        tagName="p"
+                        onChange={this.onChangeInfo}
+                        value={info}
+                        placeholder={__("Member Info", "mytheme-blocks")}
+                        formatingControls={[]}
+                    />
+                </div>
+            </>
         );
     }
 }

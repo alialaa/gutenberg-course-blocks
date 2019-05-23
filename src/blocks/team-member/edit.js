50afab61653a33258f292e835ebd4fb3c12fb9a1
digital ankit <akjaph@gmail.com>
2019-05-23 15:04:10 +0530

    made the social icons sortable using react sortable hoc

--- src/blocks/team-member/edit.js
+++ src/blocks/team-member/edit.js
@@ -23,6 +23,11 @@ import {
     TextControl
 } from "@wordpress/components";
 import { withSelect } from "@wordpress/data";
+import {
+    SortableContainer,
+    SortableElement,
+    arrayMove
+} from "react-sortable-hoc";
 
 class TeamMemberEdit extends Component {
     state = {
@@ -137,10 +142,64 @@ class TeamMemberEdit extends Component {
             selectedLink: null
         });
     };
+    onSortEnd = (oldIndex, newIndex) => {
+        const { setAttributes, attributes } = this.props;
+        const { social } = attributes;
+        let new_social = arrayMove(social, oldIndex, newIndex);
+        setAttributes({ social: new_social });
+        this.setState({ selectedLink: null });
+    };
     render() {
         //console.log(this.props);
         const { className, attributes, noticeUI, isSelected } = this.props;
         const { title, info, url, alt, id, social } = attributes;
+
+        const SortableList = SortableContainer(() => {
+            return (
+                <ul>
+                    {social.map((item, index) => {
+                        let SortableItem = SortableElement(() => {
+                            return (
+                                <li
+                                    key={index}
+                                    onClick={() =>
+                                        this.setState({
+                                            selectedLink: index
+                                        })
+                                    }
+                                    className={
+                                        this.state.selectedLink === index
+                                            ? "is-selected"
+                                            : null
+                                    }
+                                >
+                                    <Dashicon icon={item.icon} size={16} />
+                                </li>
+                            );
+                        });
+                        return <SortableItem key={index} index={index} />;
+                    })}
+                    {isSelected && (
+                        <li
+                            className={
+                                "wp-block-mytheme-blocks-team-member__addIconLI"
+                            }
+                        >
+                            <Tooltip text={__("Add Item", "mytheme-blocks")}>
+                                <button
+                                    className={
+                                        "wp-block-mytheme-blocks-team-member__addIcon"
+                                    }
+                                    onClick={this.addNewLink}
+                                >
+                                    <Dashicon icon={"plus"} size={14} />
+                                </button>
+                            </Tooltip>
+                        </li>
+                    )}
+                </ul>
+            );
+        });
         return (
             <>
                 <InspectorControls>
@@ -240,7 +299,15 @@ class TeamMemberEdit extends Component {
                             "wp-block-mytheme-blocks-team-member__social"
                         }
                     >
-                        <ul>
+                        <SortableList
+                            axis="x"
+                            helperClass={"social_dragging"}
+                            distance={10}
+                            onSortEnd={({ oldIndex, newIndex }) =>
+                                this.onSortEnd(oldIndex, newIndex)
+                            }
+                        />
+                        {/* <ul>
                             {social.map((item, index) => {
                                 return (
                                     <li
@@ -280,7 +347,7 @@ class TeamMemberEdit extends Component {
                                     </Tooltip>
                                 </li>
                             )}
-                        </ul>
+                        </ul> */}
                     </div>
                     {this.state.selectedLink !== null && (
                         <div

49a60d1c19b33272feb0f209674130345122940f
digital ankit <akjaph@gmail.com>
2019-05-24 03:52:03 +0530

    fetched the latest posts in the js side using redux

--- src/blocks/latest-posts/edit.js
+++ src/blocks/latest-posts/edit.js
@@ -1,9 +1,18 @@
 import { Component } from "@wordpress/element";
+import { withSelect } from "@wordpress/data";
 
 class LatestPostsEdit extends Component {
     render() {
+        //console.log(this.props);
         return <p> dsjljl </p>;
     }
 }
 
-export default LatestPostsEdit;
+export default withSelect((select, props) => {
+    const { attributes } = props;
+    const { numberOfPosts } = attributes;
+    let query = { per_page: numberOfPosts };
+    return {
+        posts: select("core").getEntityRecords("postType", "post", query)
+    };
+})(LatestPostsEdit);

f583e0d57b0ea2e7b70296bf176ea61c87904413
digital ankit <akjaph@gmail.com>
2019-05-25 03:07:47 +0530

    displayed our to dos from the redux store in the to do list component

--- src/blocks/todo-list/edit.js
+++ src/blocks/todo-list/edit.js
@@ -1,24 +1,64 @@
-// .. note :- THIS MARKUP COMMENTED FOR JUST COMMIT CODE WITHOUT ERROR//
+import { Component } from "@wordpress/element";
+import { withSelect, withDispatch } from "@wordpress/data";
+import { compose } from "@wordpress/compose";
 
-//import { Component } from "@wordpress/components";
-//import { withSelect, withDispatch, dispatch } from "@wordpress/data";
-//import { composer } from "@wordpress/compose";
-
-/* 
 class ReduxTodoEdit extends Component {
+    state = {
+        new_todo: ""
+    };
     render() {
-        return <div />;
+        const { todos, addToDo } = this.props;
+        return (
+            <div>
+                {todos.map((todo, index) => {
+                    return (
+                        <div
+                            key={index}
+                            style={
+                                todo.completed
+                                    ? {
+                                          textDecoration: "line-through",
+                                          opacity: 0.5
+                                      }
+                                    : undefined
+                            }
+                        >
+                            <input type="checkbox" checked={todo.completed} />
+                            {todo.title}
+                        </div>
+                    );
+                })}
+                <input
+                    type="text"
+                    value={this.state.new_todo}
+                    onChange={e => this.setState({ new_todo: e.target.value })}
+                />
+                <button
+                    onClick={() =>
+                        addToDo({
+                            title: this.state.new_todo,
+                            completed: false
+                        })
+                    }
+                >
+                    Add
+                </button>
+            </div>
+        );
     }
 }
-*/
 
-/* 
 export default compose([
     withSelect(select => {
-        return {};
+        return {
+            todos: select("mytheme-blocks/todo").getTodos()
+        };
     }),
     withDispatch(dispatch => {
-        return {};
+        return {
+            addToDo: item => {
+                dispatch("mytheme-blocks/todo").addToDo(item);
+            }
+        };
     })
 ])(ReduxTodoEdit);
-*/

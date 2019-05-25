3eeb24bf45cffcd1fc57753315faa62d259c9862
digital ankit <akjaph@gmail.com>
2019-05-25 06:52:18 +0530

    get and set metadata using redux selector and actions

--- src/metabox.php
+++ src/metabox.php
@@ -1,4 +1,19 @@
 <?php
+
+function mytheme_blocks_register_meta() {
+    register_meta('post', '_mytheme_blocks_post_subtitle', array(
+        'show_in_rest' => true,
+        'type' => 'string',
+        'single' => true,
+        'sanitize_callback' => 'sanitize_text_field',
+        'auth_callback' => function() {
+            return current_user_can('edit_posts');
+        }
+    ));     
+}
+
+add_action('init', 'mytheme_blocks_register_meta');
+
 function mytheme_blocks_add_meta_box() {
     add_meta_box( 
         'mytheme_blocks_post_options_metabox', 

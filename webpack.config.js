bacf815047e87e5f83d28af1c7f29da368b90c40
Gayrat Vlasov <33682052+Ulibka68@users.noreply.github.com>
2020-03-04 11:51:49 +0300

    update to working

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
-const autoprefixer = require("autoprefixer");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// This plugin uses terser to minify your JavaScript.
 const TerserPlugin = require("terser-webpack-plugin");
-const CleanPlugin = require("clean-webpack-plugin");
+
+// It will search for CSS assets during the Webpack build and will optimize \ minimize the CSS 
+// (by default it uses cssnano but a custom CSS processor can be specified).
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 
 module.exports = (env, argv) => {
     function isDevelopment() {
@@ -32,16 +37,21 @@ module.exports = (env, argv) => {
             ]
         },
         plugins: [
-            new CleanPlugin(),
-            new MiniCSSExtractPlugin({
-                chunkFilename: "[id].css",
-                filename: chunkData => {
-                    return chunkData.chunk.name === "script"
-                        ? "style.css"
-                        : "[name].css";
-                }
-            })
-        ],
+          
+            new MiniCssExtractPlugin({
+              chunkFilename: '[id].css',
+            //   chunk типа webpack.compilation.Chunk
+              moduleFilename: (chunk) => {
+                const { name } = chunk;
+                // chunk.entryModule.id) == './src/editor.js',
+                // name == 'editor'
+                return   name === "script" ? "style.css" : "[name].css";
+              }
+            }),
+            new CleanWebpackPlugin(),
+
+          ],
+      
         devtool: isDevelopment() ? "cheap-module-source-map" : "source-map",
         module: {
             rules: [
@@ -52,6 +62,9 @@ module.exports = (env, argv) => {
                         {
                             loader: "babel-loader",
                             options: {
+                                plugins: [
+                                    "@babel/plugin-proposal-class-properties"
+                                ],
                                 presets: [
                                     "@babel/preset-env",
                                     [
@@ -69,19 +82,14 @@ module.exports = (env, argv) => {
                     ]
                 },
                 {
-                    test: /\.(sa|sc|c)ss$/,
+                    test: /\.(sass|scss|css)$/,
                     use: [
-                        MiniCSSExtractPlugin.loader,
+                         MiniCssExtractPlugin.loader,
                         "css-loader",
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                plugins: [autoprefixer()]
-                            }
-                        },
                         "sass-loader"
                     ]
                 }
+             
             ]
         },
         externals: {
@@ -89,7 +97,8 @@ module.exports = (env, argv) => {
             "@wordpress/blocks": ["wp", "blocks"],
             "@wordpress/i18n": ["wp", "i18n"],
             "@wordpress/editor": ["wp", "editor"],
-            "@wordpress/components": ["wp", "components"]
+            "@wordpress/components": ["wp", "components"],
+            "@wordpress/element": ["wp", "element"]
         }
     };
     return config;
